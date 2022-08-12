513245d0f500cd8edc0eb1ecad1c686936dddd66
Maximilian Øystå Lloyd <me@maxlloyd.no>
2022-08-13 00:38:18 +0200

    layout ferdig

--- scripts.js
+++ scripts.js
@@ -1,1 +1,14 @@
 console.log('Hello World');
+
+
+const leftButton = document.querySelector('#left-arrow'); 
+const rightButton = document.querySelector('#right-arrow'); 
+
+
+leftButton.onclick = function() {
+    console.log('clicked left');
+}
+
+rightButton.onclick = function() {
+    console.log('clicked right');
+}
