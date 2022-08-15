39e5b6a97231d6f9b292aa0ee9bbea45a30ac20c
Maximilian Øystå Lloyd <me@maxlloyd.no>
2022-08-15 07:24:18 +0200

    med slides inne

--- scripts.js
+++ scripts.js
@@ -12,3 +12,9 @@ leftButton.onclick = function() {
 rightButton.onclick = function() {
     console.log('clicked right');
 }
+
+
+let  slides = ["./assets/image-slide-1.jpg", "./assets/image-slide-2.jpg", "./assets/image-slide-3.jpg", "./assets/image-slide-4.jpg", "./assets/image-slide-5.jpg"];
+
+let slideShow = document.querySelector('#slideShow');
+
