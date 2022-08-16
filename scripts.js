a0f5a630df69b7b861b53a9cad7305b2b31e81e8
Maximilian Øystå Lloyd <me@maxlloyd.no>
2022-08-16 23:02:08 +0200

    Desktop finished more or less

--- scripts.js
+++ scripts.js
@@ -1,14 +1,30 @@
-console.log('Hello World');
-
-
 const leftButton = document.querySelector('#left-arrow'); 
 const rightButton = document.querySelector('#right-arrow'); 
 
+let  slides = ["./assets/image-slide-1.jpg", "./assets/image-slide-2.jpg", "./assets/image-slide-3.jpg", "./assets/image-slide-4.jpg", "./assets/image-slide-5.jpg"];
+let slideShow = document.querySelector('#slideShow');
+
+slides.map((image) => {
+    let newImage = document.createElement('img');
+    newImage.src = image;
+    slideShow.appendChild(newImage);
+
+})
 
 leftButton.onclick = function() {
-    console.log('clicked left');
+
+    let firstImage = slideShow.firstChild;
+    slideShow.insertBefore(firstImage, slideShow.lastChild);
+
+
+    slideShow.firstElementChild.classList.remove('new-item');
+    slideShow.lastElementChild.classList.add('new-item');
+
+
 }
 
 rightButton.onclick = function() {
-    console.log('clicked right');
+    let lastImage = slideShow.lastChild;
+    slideShow.insertBefore(lastImage, slideShow.firstChild);
 }
+
